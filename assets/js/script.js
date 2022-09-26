4e7c2320e9627d3daf8fa99e78ed531e16b3466f
CoderGirl <angi.adema@gmail.com>
2022-09-26 12:10:59 -0600

    added funtions to js file to save and retrieve previous searches in local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 var apiKey = '7uwcgjvfwhg6h468vquck77e';
-var submitBtn = document.getElementById('submit');
-var userInput = document.getElementById('zipcode');
+var searchBtn = document.getElementById('search');
+var userInputCity = document.getElementById('city');
+var userInputDate = document.getElementById('date');
 var date ='2022-09-26';
+
+var searchHistory = [];
 
 //Create a function to pull in current and upcoming movie titles from API.
 function start(movieTitles) {
@@ -15,11 +18,7 @@ function start(movieTitles) {
             console.log(data);
         })
 
-    // function handleUserInput() {
-    //     var zipcodeInput = userInput.value.trim();
-
-    // }
-    // getMovieLocations()
+    
 }
 
 
@@ -27,6 +26,26 @@ function start(movieTitles) {
 
 start();
 
+//Create a function that handles the user input(city & date) to local storage.
+function saveToLocalStorage(cityAndDate) {
+    //Check for duplicate entries in search.
+    if (searchHistory.indexOf(cityAndDate) !== -1) {
+    }
+    //Push city and date search history to the searchHistory array.
+    searchHistory.push(cityAndDate);
+    //Save search history array into local storage.
+    localStorage.setItem('searchHistory', JSON.stringify('searchHistory'));
+    saveHistoryButtons();
+}
 
+//Function to retrieve saved data in local storage.
+function retrieveLocalStorage() {
+    var history = localStorage.getItem('searchHistory');
+    if (history) {
+        searchHistory = JSON.parse(history)
+    }
+    saveHistoryButtons();
+}
+retrieveLocalStorage();
 
 
